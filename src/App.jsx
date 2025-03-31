2ffd5179a16701482b0c84b300a0b1b224903c12
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-04-01 00:15:21 +0530

    onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const App = () => {
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.lang = "en-US";
+  recognition.interimResults = false;
+
+  const recordButton = () => {
+    recognition.start();
+  };
   return (
     <div className="min-h-screen w-full bg-black flex items-center justify-center text-white">
-      <button className="px-6 py-3 bg-gray-200 text-black font-semibold rounded-full hover:bg-gray-300 transform hover:scale-105 transition-all duration-200 shadow-lg hover:shadow-xl active:scale-95">
+      <button
+        onClick={recordButton}
+        className="px-6 py-3 bg-gray-200 text-black font-semibold rounded-full hover:bg-gray-300 transform hover:scale-105 transition-all duration-200 shadow-lg hover:shadow-xl active:scale-95"
+      >
         Record
       </button>
     </div>
